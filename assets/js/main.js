6c10cce8aea903e4c18bfef9260ca11275f1880c
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-10-17 22:05:55 +0600

    working for frontend development

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,41 +28,52 @@ const alapRoom = document.getElementById("alapRoom");
 const alochonaRoom = document.getElementById("alochonaRoom");
 // -------------------------------requiredDomOfWorkSpaceEnd-----------------------------
 
-// ----------------------------requiredDomOfSelectedPlanStart-----------------------------
+// ----------------------------requiredDomOfSelectedOfficeOrRoomOverviewStart-----------------------------
 const checkedRadio = document.getElementsByName('chosen_plan');
-
-// -------------------------------requiredDomOfChosenPlanOverview-----------------------------
+const selectedOfficeOrRoom = document.getElementById('selectedOfficeOrRoom');
+const selectedPlanTime = document.getElementById('selectedPlanTime');
+const selectedPlanOverview = document.getElementById('selectedPlanOverview');
 // -------------------------------requiredDomOfWorkspaceOptions-----------------------------
-const privateWorkspaceOptions = document.getElementsByName('privateWorkspaceOptions');
-const sharedWorkspaceOptions = document.getElementsByName('sharedWorkspaceOptions');
-const collaborativeWorkspaceOptions = document.getElementsByName('collaborativeWorkspaceOptions');
+const privateWorkspaceOptions = document.getElementById('privateWorkspaceOptions');
+const sharedWorkspaceOptions = document.getElementById('sharedWorkspaceOptions');
+const collaborativeWorkspaceOptions = document.getElementById('collaborativeWorkspaceOptions');
 // -------------------------------requiredDomOfNumberOfPeople-----------------------------
-const people = document.getElementsByName('people');
-const zero = document.getElementsByName('zero');
-const decrement = document.getElementsByName('decrement');
-const increment = document.getElementsByName('increment');
+const people = document.getElementById('people');
+const zero = document.getElementById('zero');
+const decrement = document.getElementById('decrement');
+const increment = document.getElementById('increment');
 // -------------------------------requiredDomOfDate-----------------------------
-const date = document.getElementsByName('date');
+const date = document.getElementById('date');
 // -------------------------------requiredDomOfcheckBoxNBackNContinueBtn-------
-const chosenPlanBackBtn = document.getElementsByName('chosenPlanBackBtn');
-const chosenPlanContinueBtn = document.getElementsByName('chosenPlanContinueBtn');
+const chosenPlanBackBtn = document.getElementById('chosenPlanBackBtn');
+const chosenPlanContinueBtn = document.getElementById('chosenPlanContinueBtn');
 
-// ----------------------------requiredDomOfSelectedPlanEnd-----------------------------
+// ----------------------------requiredDomOfSelectedOfficeOrRoomOverviewEnd-----------------------------
 
 // ----------------------------requiredDomOfPersonalDetailsStart-----------------------------
-const personalDetails = document.getElementsByName('personalDetails');
-const termsAndConditions = document.getElementsByName('termsAndConditions');
-const personalDetailsBackBtn = document.getElementsByName('personalDetailsBackBtn');
-const personalDetailsContinueBtn = document.getElementsByName('personalDetailsContinueBtn');
+const personalDetails = document.getElementById('personalDetails');
+const firstName = document.getElementById('firstName').value;
+const lastName = document.getElementById('lastName').value;
+const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
+const country = document.getElementById('country').value;
+const companyName = document.getElementById('companyName').value;
+const address = document.getElementById('address').value;
+const apartment = document.getElementById('apartment').value;
+const cityTown = document.getElementById('cityTown').value;
+const postcode = document.getElementById('postcode').value;
+const termsAndConditions = document.getElementById('termsAndConditions');
+const personalDetailsBackBtn = document.getElementById('personalDetailsBackBtn');
+const personalDetailsContinueBtn = document.getElementById('personalDetailsContinueBtn');
 // ----------------------------requiredDomOfPersonalDetailsEnd-----------------------------
 
 // ----------------------------requiredDomOfReviewPaymentDetailsStart-----------------------------
-const reviewPaymentDetails = document.getElementsByName('reviewPaymentDetails');
-const reviewPaymentDetailsBackBtn = document.getElementsByName('reviewPaymentDetailsBackBtn');
-const submit = document.getElementsByName('submit');
+const reviewPaymentDetails = document.getElementById('reviewPaymentDetails');
+const reviewPaymentDetailsBackBtn = document.getElementById('reviewPaymentDetailsBackBtn');
+const submit = document.getElementById('submit');
 // ----------------------------requiredDomOfPaymentMethodsStart----------
-const payNow = document.getElementsByName('payNow');
-const payOnArrival = document.getElementsByName('payOnArrival');
+const payNow = document.getElementById('payNow');
+const payOnArrival = document.getElementById('payOnArrival');
 // ----------------------------requiredDomOfPaymentMethodsEnd----------
 
 // ----------------------------requiredDomOfReviewPaymentDetailsEnd-----------------------------
